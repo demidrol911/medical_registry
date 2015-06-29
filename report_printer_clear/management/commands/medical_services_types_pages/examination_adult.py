92833daaecdc1a948b1f1a925a44c4c45484d2ca
demidrol911 <dam91191@gmail.com>
2015-06-29 12:05:59 +1000

    Добавил новые отчетные формы СВОДНАЯ ИНФОРМАЦИЯ об объеме и стоимости

---
+++ report_printer_clear/management/commands/medical_services_types_pages/examination_adult.py
@@ -0,0 +1,175 @@
+from general import MedicalServiceTypePage
+
+
+class ExamAdultFirstStagePage(MedicalServiceTypePage):
+
+    def __init__(self):
+        self.data = None
+        self.page_number = 0
+
+    def get_query(self):
+        query = MedicalServiceTypePage.get_general_query() + '''
+                SELECT
+                    mo_code AS mo_code,
+                    CASE WHEN ms_final.code IN ('019021', '019022')
+                           THEN '1'
+                        WHEN ms_final.code IN ('019023', '019024')
+                           THEN '2'
+                    END ||
+                    CASE WHEN service_subgroup is NULL
+                           THEN '&' || service_code
+                         ELSE ''
+                    END AS group_field,
+
+                    COUNT(DISTINCT patient_id) AS count_patients,
+                    COUNT(DISTINCT CASE WHEN patient_gender = 1
+                                          THEN patient_id
+                                   END) AS count_patients_male,
+                    COUNT(DISTINCT CASE WHEN patient_gender = 2
+                                          THEN patient_id
+                                   END) AS count_patients_female,
+
+                    COUNT(DISTINCT service_id) AS count_services,
+                    COUNT(DISTINCT CASE WHEN patient_gender = 1
+                                          THEN service_id
+                                   END) AS count_services_male,
+                    COUNT(DISTINCT CASE WHEN patient_gender = 2
+                                          THEN service_id
+                                   END) AS count_services_female,
+
+                    SUM(service_tariff) AS total_tariff,
+                    SUM(CASE WHEN patient_gender = 1
+                               THEN service_tariff
+                             ELSE 0
+                        END) AS total_tariff_male,
+                    SUM(CASE WHEN patient_gender = 2
+                               THEN service_tariff
+                             ELSE 0
+                        END) AS total_tariff_female,
+
+                    SUM(CASE WHEN psc.id_pk is NOT NULL
+                               THEN ROUND(service_tariff*0.07, 2)
+                             ELSE 0 END) AS coeff1_07,
+                    SUM(CASE WHEN psc.id_pk is NOT NULL AND patient_gender = 1
+                               THEN ROUND(service_tariff*0.07, 2)
+                             ELSE 0
+                        END) AS coeff1_07_male,
+                    SUM(CASE WHEN psc.id_pk is NOT NULL AND patient_gender = 2
+                               THEN ROUND(service_tariff*0.07, 2)
+                             ELSE 0
+                        END) AS coeff1_07_female,
+
+                    SUM(service_accepted) AS total_accepted,
+                    SUM(CASE WHEN patient_gender = 1
+                               THEN service_accepted
+                             ELSE 0
+                        END) AS total_accepted_male,
+                    SUM(CASE WHEN patient_gender = 2
+                               THEN service_accepted
+                             ELSE 0
+                        END) AS total_accepted_female
+
+                FROM registry_services
+                    LEFT JOIN provided_service_coefficient psc
+                      ON psc.service_fk = service_id
+                      AND psc.coefficient_fk = 5
+                    lEFT JOIN medical_service ms_final
+                      ON ms_final.id_pk = (
+                         SELECT ms1.id_pk
+                         FROM provided_service ps1
+                             JOIN medical_service ms1
+                                ON ms1.id_pk = ps1.code_fk
+                         WHERE ps1.event_fk = event_id
+                               AND ms1.code in (
+                                   '019021',
+                                   '019023',
+                                   '019022',
+                                   '019024'
+                               )
+                      )
+                WHERE service_group = 7
+                      AND service_code IN (
+                          '019021',
+                          '019023',
+                          '019022',
+                          '019024',
+                          '019001',
+                          '019020'
+                      )
+                GROUP BY mo_code, group_field
+                '''
+        return query
+
+    def get_output_order_fields(self):
+        fields_final = ('count_patients',
+                        'count_patients_male',
+                        'count_patients_female',
+
+                        'count_services',
+                        'count_services_male',
+                        'count_services_female',
+
+                        'total_tariff',
+                        'total_tariff_male',
+                        'total_tariff_female',
+
+                        'coeff1_07',
+                        'coeff1_07_male',
+                        'coeff1_07_female',
+
+                        'total_accepted',
+                        'total_accepted_male',
+                        'total_accepted_female')
+        fields_primary = ('count_patients',
+                          'count_patients_male',
+                          'count_patients_female',
+
+                          'count_services',
+                          'count_services_male',
+                          'count_services_female')
+        return (('1', 4, fields_final),
+                ('2', 21, fields_final),
+                ('1&019001', 52, fields_primary),
+                ('2&019001', 58, fields_primary),
+                ('1&019020', 71, fields_primary),
+                ('2&019020', 77, fields_primary))
+
+
+class ExamAdultSecondStagePage(MedicalServiceTypePage):
+
+    def __init__(self):
+        self.data = None
+        self.page_number = 1
+
+    def get_query(self):
+        query = MedicalServiceTypePage.get_general_query() + '''
+                SELECT
+                    mo_code AS mo_code,
+                    service_code AS group_field,
+                    COUNT(DISTINCT patient_id) AS count_patients,
+                    COUNT(DISTINCT service_id) AS count_services,
+                    SUM(service_tariff) AS total_tariff
+                FROM registry_services
+                WHERE service_group IN (25, 26)
+                GROUP BY mo_code, group_field
+                '''
+        return query
+
+    def get_output_order_fields(self):
+        fields = ('count_patients', 'count_services', 'total_tariff')
+        return (('019102', 3, fields),
+                ('019103', 7, fields),
+                ('019104', 11, fields),
+                ('019106', 15, fields),
+                ('019107', 19, fields),
+                ('019105', 23, fields),
+                ('019116', 27, fields),
+                ('019108', 31, fields),
+                ('019109', 35, fields),
+                ('019110', 39, fields),
+                ('019111', 43, fields),
+                ('019112', 47, fields),
+                ('019113', 51, fields),
+                ('019114', 55, fields),
+                ('019115', 59, fields),
+                ('019117', 63, fields))
---
+++ report_printer_clear/management/commands/medical_services_types_pages/general.py
@@ -0,0 +1,94 @@
+from abc import abstractmethod
+
+from django.db import connection
+
+from main.funcs import dictfetchall
+from main.funcs import howlong
+from const import POSITION_REPORT
+from report_printer_clear.utils.excel_style import VALUE_STYLE
+from report_printer_clear.utils.page import ReportPage
+
+
+class MedicalServiceTypePage(ReportPage):
+    def __init__(self):
+        self.data = None
+        self.page_number = 0
+
+    @howlong
+    def calculate(self, parameters):
+        self.data = None
+        query = self.get_query()
+        cursor = connection.cursor()
+        cursor.execute(query, dict(year=parameters.registry_year,
+                                   period=parameters.registry_period))
+        self.data = dictfetchall(cursor)
+        cursor.close()
+
+    def print_page(self, sheet, parameters):
+        sheet.set_style(VALUE_STYLE)
+        for service_group, position, order_field in self.get_output_order_fields():
+            current_items = []
+            for item in self.data:
+                if item['group_field'] == service_group:
+                    current_items.append(item)
+
+            for item in current_items:
+                sheet.set_position(POSITION_REPORT[item['mo_code']], position)
+                for field in order_field:
+                    sheet.write(item[field], 'c')
+
+    @abstractmethod
+    def get_query(self):
+        pass
+
+    @staticmethod
+    def get_general_query():
+        query = '''
+                WITH registry_services AS (
+                    SELECT
+                        pt.gender_fk AS patient_gender,
+                        pe.term_fk AS service_term,
+                        ms.tariff_profile_fk AS service_tariff_profile,
+                        ms.group_fk AS service_group,
+                        ms.subgroup_fk AS service_subgroup,
+                        ms.code ILIKE '0%%' AS is_adult,
+                        ms.code AS service_code,
+
+                        CASE WHEN ms.group_fk = 19
+                               THEN ms.uet * ps.quantity
+                             ELSE ps.quantity
+                        END AS service_quantity,
+
+                        ps.id_pk AS service_id,
+                        pe.id_pk AS event_id,
+                        ROUND(ps.tariff, 2) AS service_tariff,
+                        ROUND(ps.accepted_payment, 2) AS service_accepted,
+                        mr.organization_code AS mo_code,
+                        pt.id_pk AS patient_id,
+                        ps.start_date AS service_start_date,
+                        ps.end_date AS service_end_date
+                    FROM medical_register mr
+                        JOIN medical_register_record mrr
+                           ON mr.id_pk = mrr.register_fk
+                        JOIN provided_event pe
+                           ON mrr.id_pk = pe.record_fk
+                        JOIN provided_service ps
+                           ON ps.event_fk = pe.id_pk
+                        JOIN medical_organization mo
+                           ON ps.organization_fk = mo.id_pk
+                        JOIN medical_service ms
+                           ON ms.id_pk = ps.code_fk
+                        JOIN patient pt
+                           ON pt.id_pk = mrr.patient_fk
+                        WHERE mr.is_active
+                            AND mr.period = %(period)s
+                            AND mr.year = %(year)s
+                            AND ps.payment_type_fk = 2
+                            AND (ms.group_fk != 27 OR ms.group_fk is NULL)
+                )
+                '''
+        return query
+
+    @abstractmethod
+    def get_output_order_fields(self):
+        pass
