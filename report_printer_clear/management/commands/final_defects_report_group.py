d232651397bb7b29fdd329efba9123b08313bfd6
demidrol911 <dam91191@gmail.com>
2016-02-25 17:38:39 +1000

    Сделал общий отчёт по дефектам, в котором больницы сгруппированы по районам

---
+++ report_printer_clear/management/commands/final_defects_report_group.py
@@ -0,0 +1,23 @@
+#! -*- coding: utf-8 -*-
+from django.core.management.base import BaseCommand
+from defects_report_pages.final_defects_group import FinalDefectsGroupPage
+from report_printer_clear.utils.report import Report
+from report_printer_clear.utils.report import ReportParameters
+from medical_service_register.path import REESTR_DIR
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        report = Report(template='defects_group.xls', suffix=u'дефекты')
+        report.add_page(FinalDefectsGroupPage())
+        parameters = ReportParameters()
+
+        path_to_dir = REESTR_DIR
+        parameters.path_to_dir = path_to_dir % (
+            parameters.registry_year,
+            parameters.registry_period
+        )
+        parameters.report_name = u'дефекты_общий_по_группам'
+
+        report.print_pages(parameters)
